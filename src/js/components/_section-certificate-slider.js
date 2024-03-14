e09582a9b195961d465219c9e6e5f424d587e179
Alexey <aaa@mworx.ru>
2024-03-14 12:56:08 +0300

    initial comment

--- src/js/components/_section-certificate-slider.js
+++ src/js/components/_section-certificate-slider.js
@@ -3,39 +3,79 @@ import {Navigation, Pagination} from "swiper/modules";
 import lightGallery from "lightgallery";
 
 document.addEventListener('DOMContentLoaded', () => {
+
+	const initializeSliderAndGallery = (certificate) => {
+		new Swiper(certificate, {
+			modules: [Navigation, Pagination],
+			slidesPerView: "auto",
+			grabCursor: true,
+			navigation: {
+				prevEl: '.section-certificate-slider__nav .slider-nav__btn--prev',
+				nextEl: '.section-certificate-slider__nav .slider-nav__btn--next',
+			},
+			breakpoints: {
+				320: {
+					spaceBetween: 16
+				},
+				640: {
+					spaceBetween: 30
+				}
+			},
+			on: {
+				init: function () {
+					const lgItem = certificate.querySelector('.swiper-wrapper');
+					if (lgItem) {
+						lightGallery(lgItem, {
+							selector: '.section-certificate-slider__elem',
+						});
+					}
+
+				},
+			}
+		});
+	}
+
+	const initializeSliderAndGalleryModal = (certificate) => {
+		new Swiper(certificate, {
+			modules: [Navigation, Pagination],
+			slidesPerView: "auto",
+			grabCursor: true,
+			navigation: {
+				prevEl: '.section-certificate-slider__nav-modal .slider-nav__btn--prev',
+				nextEl: '.section-certificate-slider__nav-modal .slider-nav__btn--next',
+			},
+			breakpoints: {
+				320: {
+					spaceBetween: 16
+				},
+				640: {
+					spaceBetween: 30
+				}
+			},
+			on: {
+				init: function () {
+					const lgItem = certificate.querySelector('.swiper-wrapper');
+					if (lgItem) {
+						lightGallery(lgItem, {
+							selector: '.section-certificate-slider__elemModal',
+						});
+					}
+				},
+			}
+		});
+	}
+
 	const certificates = document.querySelectorAll('.section-certificate-slider__content');
+
 
 	if (certificates.length) {
 		certificates.forEach((certificate) => {
-			new Swiper(certificate, {
-				modules: [Navigation, Pagination],
-				slidesPerView: "auto",
-				grabCursor: true,
-				navigation: {
-					prevEl: '.section-certificate-slider__nav .slider-nav__btn--prev',
-					nextEl: '.section-certificate-slider__nav .slider-nav__btn--next',
-				},
-				breakpoints: {
-					320: {
-						spaceBetween: 16
-					},
-					640: {
-						spaceBetween: 30
-					}
-				},
-				on: {
-					init: function () {
-						const lgItems = document.querySelectorAll('.section-certificate-slider__content .swiper-wrapper');
-						if (lgItems.length) {
-							lgItems.forEach((lgItem) => {
-								lightGallery(lgItem,{
-									selector: '.section-certificate-slider__elem',
-								});
-							})
-						}
-					},
-				}
-			});
+			const modal = certificate.closest('.modal');
+			if (!modal) {
+				initializeSliderAndGallery(certificate);
+			} else {
+				initializeSliderAndGalleryModal(certificate)
+			}
 		})
 	}
 
